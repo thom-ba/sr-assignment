2744061d6e63278ad60c8d40e3d41388036fe90e
Thomas Baumeister <at.thomb@gmail.com>
2025-10-31 23:34:51 +0100

    added not null constraint to Event model

--- backend/models/event.go
+++ backend/models/event.go
@@ -3,13 +3,13 @@ package models
 import "time"
 
 type Event struct {
-	ID            uint `gorm:"primaryKey; autoIncrement"`
-	CompetitionId uint
-	EventTypeId   uint
-	HomeTeamId    uint
-	AwayTeamId    uint
-	VenueId       uint
-	EventDateTime time.Time
-	Name          string
-	Description   string
+	ID            uint      `gorm:"primaryKey; autoIncrement"`
+	CompetitionId uint      `gorm:"not null"`
+	EventTypeId   uint      `gorm:"not null"`
+	HomeTeamId    uint      `gorm:"not null"`
+	AwayTeamId    uint      `gorm:"not null"`
+	VenueId       uint      `gorm:"not null"`
+	EventDateTime time.Time `gorm:"not null"`
+	Name          *string
+	Description   *string
 }
