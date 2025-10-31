d9f7a0bc2b26f08fdfef429b8bd26fd78426738c
Thomas Baumeister <at.thomb@gmail.com>
2025-10-31 23:32:20 +0100

    fixed not null constraint in competition.go

--- backend/models/competition.go
+++ backend/models/competition.go
@@ -1,9 +1,9 @@
 package models
 
 type Competition struct {
-	ID         uint `gorm:"primaryKey; autoIncrement"`
-	SportID    uint `gorm:"index:idx_sport_name_year,unique"`
-	CategoryId uint
-	Name       string `gorm:"index:idx_sport_name_year,unique"`
-	Year       int    `gorm:"index:idx_sport_name_year,unique"`
+	ID         uint   `gorm:"primaryKey; autoIncrement"`
+	SportID    uint   `gorm:"not null; index:idx_sport_name_year,unique"`
+	CategoryId uint   `gorm:"not null"`
+	Name       string `gorm:"not null; index:idx_sport_name_year,unique"`
+	Year       int    `gorm:"not null; index:idx_sport_name_year,unique"`
 }
