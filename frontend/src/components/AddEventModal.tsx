137064fbcadc96d6c0383f130d4957edeafd3c7b
Thomas Baumeister <at.thomb@gmail.com>
2025-11-07 12:05:18 +0100

    Fixed button component (had div instead of button from the begin), added New Sport to the AddEventModal

--- frontend/src/components/AddEventModal.tsx
+++ frontend/src/components/AddEventModal.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Sport, Venue } from "../types";
 import { Modal } from "./ui/Modal";
 import { Input } from "./ui/Input";
+import { CirclePlus } from "lucide-react";
+import { Button } from "./ui/Button";
 
 
 interface AddEventModalProps {
@@ -21,12 +23,18 @@ export const AddModalEvent: React.FC<AddEventModalProps> = ({ onClose, onAddEven
         awayTeam: '',
         description: '',
     })
+    const [isAddingNewSport, setIsAddingNewSport] = useState(false)
+    const [newSportName, setNewSportName] = useState('')
 
     const handleSubmit = () => { }
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         const { name, value } = e.target;
         setFormData(prev => ({ ...prev, [name]: value }))
+    }
+
+    const handleSaveNewSport = () => {
+
     }
 
     return (
@@ -37,7 +45,39 @@ export const AddModalEvent: React.FC<AddEventModalProps> = ({ onClose, onAddEven
                     <Input id="awayTeam" name="awayTeam" label="Away Team" value={formData.awayTeam} onChange={handleChange} />
                 </div>
                 <Input id="dateTime" name="dateTime" label="Date and Time" type="datetime-local" value={formData.dateTime} onChange={handleChange}></Input>
+                <div className="grid grid-cols-2 items-center mb-1">
+                    <div>
+                        <div className="flex justify-between items-center pt-4 pb-1">
+                            <label htmlFor="sportId" className="text-sm font-medium text-gray-300">Sport</label>
+                            {!isAddingNewSport && (
+                                <button type="button" onClick={() => setIsAddingNewSport(true)} className="flex items-center gap-1 text-sm text-emerald-500">
+                                    <CirclePlus className="w-4 h-4" />
+                                    New
+                                </button>
+                            )}
+                        </div>
+                        {!isAddingNewSport ? (
+                            <div>
+                                <select name="sportId" id="sportId" value={formData.sportId} onChange={handleChange} className="w-full bg-gray-700 py-1 px-1 rounded-sm border border-gray-600">
+                                    {sports.map(sport => <option key={sport.id} value={sport.id}>{sport.name}</option>)}
+                                </select>
+                            </div>
+                        ) : (
+                            <div className="bg-gray-700 px-2 rounded-lg border border-gray-600">
+                                <Input id="newSport" name="newSport" label="New Sport Name" value={newSportName} onChange={(e) => setNewSportName(e.target.value)} />
+                                <div className="flex justify-end gap-2 pb-1">
+                                    <Button variant="primary" type="button" size="small" onClick={() => setIsAddingNewSport(false)}>
+                                        Cancel
+                                    </Button>
+                                    <Button type="button" variant="primary" size="small" onClick={(handleSaveNewSport)}>
+                                        Save Sport
+                                    </Button>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
             </form>
-        </Modal>
+        </Modal >
     )
 }
--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -7,9 +7,18 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export const Button: React.FC<ButtonProps> = ({ variant = 'primary', size = 'normal', children, ...props }) => {
-    return (
-        <div>
+    const baseClasses = "font-bold rounded-lg transition-transform transform hover:scale-105 focus:outline-none";
 
-        </div>
+    const variantClasses = "bg-gray-800 hover:bg-[#00003c] text-white focus:ring-gray-100 mt-2 border border-gray-600";
+
+    const sizeClasses = {
+        normal: "py-2 px-4",
+        small: "py-1 px-2 text-sm",
+    };
+
+    return (
+        <button className={`${baseClasses} ${variantClasses} ${sizeClasses[size]}`} {...props}>
+            {children}
+        </button>
     )
 }
