9a33e5cb3d1537565ba2704ba4f061f6b2e3146b
Thomas Baumeister <at.thomb@gmail.com>
2025-11-13 06:35:37 +0100

    should make one command deploy possible :pray_emoji:

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,31 +3,14 @@ package main
 import (
 	"backend/controllers"
 	database "backend/database"
-	"backend/models"
 	"backend/repositories"
 	"backend/services"
-	"log"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	db := database.GetDB()
-	err := db.AutoMigrate(
-		&models.Category{},
-		&models.Sport{},
-		&models.EventType{},
-		&models.Team{},
-		&models.Venue{},
-		&models.Competition{},
-		&models.Event{},
-	)
-	if err != nil {
-		log.Fatal("Migration failed:", err)
-	}
-	log.Println("Database migrated successfully!")
-
 	eventRepo := repositories.NewEventRepo(database.GetDB())
 	eventService := services.NewEventService(eventRepo)
 	eventController := controllers.NewEventController(eventService)
